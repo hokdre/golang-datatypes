ec85193cc6362e5e55be2ac9e7ff9f0d9839e60c
hokdre <hokdre1998@gmail.com>
2020-06-18 15:25:51 +0700

    boolean types

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # golang-datatypes
 
 * ## Basic Types
-  1. ### Bool Types
-  2. ### String Types
+  1. ### Bool Types [ def : false ]
+  2. ### String Types [ def : "" ]
   3. ### Numeric Types
-     1. #### int
+     1. #### int [ def : 0 ]
          * Unsigned
              1. uint8 (0 to 255)
              2. uint16 (0 to 65535)
@@ -15,7 +15,7 @@
              2. int16 (-32768 to 32767)
              3. int32 (-2147483648 to 2147483647)
              4. int64 (-9223372036854775808 to 9223372036854775807)
-     2. #### float
+     2. #### float [ def : 0.0 ]
          1. float32
          2. float64
          3. complex64
@@ -24,12 +24,12 @@
      4. #### rune (int32)
      5. #### uint (32 or 64 bits)
      6. #### int (uint)
-* ## Aggregate Types
+* ## Aggregate Types [ def : nil ]
       1. Array
       2. Structs
-* ## Reference Types
+* ## Reference Types [ def : nil ]
       1. Pointers
       2. Slices
       3. Map
-* ## Interface Types
+* ## Interface Types [def : nil ]
   
--- basictypes/boolean/bool_type.go
+++ basictypes/boolean/bool_type.go
@@ -1,2 +1,48 @@
-package basictypes
+package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	// 1. Declare
+	fmt.Println("1. DECLARE")
+	var boolVal bool // false
+	fmt.Printf("zero val bool varibel : %t \n \n", boolVal)
+
+	//2. Operation
+	fmt.Println("2. OPERATION")
+	fmt.Println("2.1 Operation AND (&&)")
+	fmt.Printf("true && true : %t \n", (true && true)) // true
+
+	fmt.Printf("true && false : %t \n", (true && false)) // false
+
+	fmt.Printf("false && false : %t \n", (false && false)) // false
+
+	fmt.Println("2.2 Operation OR ( || )")
+	fmt.Printf("true || true : %t \n", (true || true)) // true
+
+	fmt.Printf("true || false : %t \n", (true || false)) // true
+
+	fmt.Printf("false || false : %t \n", (false || false)) // false
+
+	fmt.Println("2.3 Operation Not ( ! )")
+
+	fmt.Printf("!(true) : %t \n", !(true)) // false
+
+	fmt.Printf("!(false) : %t \n \n", !(false)) // true
+
+	//3. Convertion
+	fmt.Println("3. CONVERTION")
+
+	fmt.Printf("Convert using strconv.FormatBool : %s \n", strconv.FormatBool(true)) // "true"
+
+	fmt.Printf("Convert using fmt.Sprintf : %s \n", fmt.Sprintf("%t", true)) // "true"
+
+	fmt.Printf("Convert using fmt.Sprintf : %s \n \n", fmt.Sprintf("%v", true)) // "true"
+
+	//4. Printing
+	fmt.Println("4. PRINTING")
+	fmt.Printf("use t code for printing : %t \n", true) //"true"
+}
